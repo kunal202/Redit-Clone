16d5376b1fd2e1a2bd718d54f9a392af33023a51
Kunal202 <kunalchandel212@gmail.com>
2020-12-31 16:15:48 +0530

    Updated Domain Name2

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ const main = async () => {
         httpOnly: true,
         secure: prod,
         sameSite: "lax",
-        domain: prod ? ".kunal202.vercel.app" : undefined,
+        domain: "https://reddit.kunal202.vercel.app/",
       },
       saveUninitialized: false,
       secret: process.env.SESSION_SECRET,
