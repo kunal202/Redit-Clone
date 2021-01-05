66e73e4635dc78cba4e6e9664490ca55770dd65c
Kunal202 <kunalchandel212@gmail.com>
2021-01-05 23:01:21 +0530

    Change Domain Name

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ const main = async () => {
         httpOnly: true,
         secure: prod,
         sameSite: "none",
-        domain: ".kunalvunal.ml",
+        domain: "kunalvunal.ml",
       },
       saveUninitialized: false,
       secret: process.env.SESSION_SECRET,
